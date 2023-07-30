e7f63eaffd4c2f4ce6b550eb1f8b129e2d0dcf3b
Emma Maksimava <cielvibritania@gmail.com>
2023-07-30 18:59:49 +0200

    add Phone Mask

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* Phone Mask */
+
+mask('[data-phone-input]');
+
+const phoneInputs = document.querySelectorAll('[data-phone-input]');
+
+phoneInputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    if(input.value == '+') input.value = '';
+  })
+
+  input.addEventListener('blur', () => {
+    if(input.value == '+') input.value = '';
+  })
+})
